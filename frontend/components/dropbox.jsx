8747b76e5543bb572986848a2777c0977267a90b
Alex E Scott <aescott@outlook.com>
2018-06-20 16:17:40 -0700

    initial drop and click no styling

--- frontend/components/dropbox.jsx
+++ frontend/components/dropbox.jsx
@@ -10,47 +10,31 @@ export default class DropBox extends Component {
     this.state = {progress: 0, errors: "", preview: "", file: ""};
   }
 
-  fileInput = React.createRef();
   dropZone = React.createRef();
 
-  // componentDidMount() {
-  //   this.dropZone.current.addEventListener('dragover', this.handleOver);
-  //   this.dropZone.current.addEventListener('dragleave', this.handleLeave);
-  //   this.dropZone.current.addEventListener('drop', this.handleDrop);
-  // }
-  handleOver
-  handleEnter = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  makeFormData(file) {
+    let formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", process.env.CLOUDINARY_PRESET);
+    formData.append("tags", ["GalleryChallenge"]);
+    return formData;
   }
-
-  handleDrop = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    console.log(e.nativeEvent);
-  }
-
-
 
   sendToCloudinary = (file) => async () => {
     if (file === "") {
-      this.setState({errors: "please first select a file"});
-      return;
+      this.setState({errors: "please first select a file"}); return;
     }
     let url = `https://api.cloudinary.com/v1_1/dol1mm8bd/auto/upload/`;
-    let formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", process.env.CLOUDINARY_PRESET);
-      formData.append("tags", ["GalleryChallenge"]);
+    let formData = this.makeFormData(file);
     let headers = { "X-Requested-With": "XMLHttpRequest" };
     let onUploadProgress = (pe) => {
       let progress = Math.round(pe.loaded / pe.total * 100);
       this.setState({progress});
     };
     let options = {headers, onUploadProgress};
-
     let r = await axios.post(url, formData, options);
     console.log(r);
+    window.URL.revokeObjectURL(this.state.preview);
     this.setState({progress: 0, preview: "", file: ""});
   }
 
@@ -62,8 +46,7 @@ export default class DropBox extends Component {
     });
   }
 
-  checkDimensions = async (file) => {
-    let url = window.URL.createObjectURL(file);
+  checkDimensions = async (file, url) => {
     let img = await this.loadImage(url);
     let height = img.naturalHeight;
     let width = img.naturalWidth;
@@ -72,7 +55,7 @@ export default class DropBox extends Component {
     return condition;
   }
 
-  checkImage = async (file) => {
+  checkImage = async (file, url) => {
     try {
       if (file.size > 1250000) {
         throw new Error('image is too big');
@@ -80,7 +63,7 @@ export default class DropBox extends Component {
       if (!['image/jpeg', 'image/gif', 'image/png'].includes(file.type)) {
         throw new Error('image is not the right type');
       }
-      let dimensions = await this.checkDimensions(file);
+      let dimensions = await this.checkDimensions(file, url);
       if (!dimensions) {
         throw new Error('height and width must be less than 1000px');
       }
@@ -90,16 +73,11 @@ export default class DropBox extends Component {
     return false;
   }
 
-  handleSubmit = async (e) => {
-    let file = this.fileInput.current.files[0];
-    let errors = await this.checkImage(file);
-    if (errors) {this.setState({errors});}
-    else {this.setState({file});}
-  }
-
-  onDrop = async (files) => {
+  handleSubmit = async (files) => {
     let file = files[0];
-    let errors = await this.checkImage(file);
+    let url = file.preview;
+    console.log(file);
+    let errors = await this.checkImage(file, url);
     if (errors) {this.setState({errors});}
     else {this.setState({file});}
   }
@@ -115,9 +93,10 @@ export default class DropBox extends Component {
     return (
       <div className="dropbox">
         <Dropzone
+          ref={this.dropZone}
           className="dropzone"
           disableClick={true}
-          onDrop={thing => this.onDrop(thing)}
+          onDrop={files => this.handleSubmit(files)}
           >
           <div className="errors">{errors}</div>
           <img
@@ -132,15 +111,19 @@ export default class DropBox extends Component {
             <div className="drop-text">Drag or Drop Image</div>
             <img src={upload}></img>
             <div className="drop-text">Or</div>
-            <input
+            {/* <input
               id="file"
               type="file"
               className="inputfile"
               ref={this.fileInput}
               onChange={this.handleSubmit}
               accept={'.jpg, .png, .gif'}
-            />
-            <label htmlFor="file">Click to Upload</label>
+            /> */}
+            {/* <label htmlFor="file">Click to Upload</label> */}
+            <label
+              onClick={() => {this.dropZone.current.open();}}>
+              Click to Upload
+            </label>
           </div>
         </Dropzone>
         <button
